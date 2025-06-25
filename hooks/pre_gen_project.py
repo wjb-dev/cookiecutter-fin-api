69632cb70834de31e5426458309f7b782b155ba7
burkswill2 <burks.113@osu.edu>
2025-06-24 21:05:47 -0400

    Added create repo toggle

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,20 @@
+from cookiecutter.main import cookiecutter
+
+LANG_MAP = {
+    "Python - FastAPI":  ("python-fastapi", "py"),
+    "Java - SpringBoot": ("java-springboot", "java"),
+    "Go - gRPC - Protoc":("go-grpc-protoc", "go"),
+    "csharp":            ("csharp", "cs"),
+}
+
+choice = "{{ cookiecutter.language }}"                     # <- ask() or argparse, etc.
+slug, abbrev = LANG_MAP[choice]
+
+cookiecutter(
+    'cookiecutter-fin-api',
+    no_input=True,
+    extra_context={
+        "variant":    slug,
+        "language_abbrev":  abbrev,
+    },
+)
