fefa525bdc3297f54294647118dae545a86cfa40
burkswill2 <burks.113@osu.edu>
2025-06-24 22:19:23 -0400

    Added create repo toggle

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,4 +1,4 @@
-from cookiecutter.main import cookiecutter
+from cookiecutter.config import get_user_config
 
 LANG_MAP = {
     "Python - FastAPI":  ("python-fastapi", "py"),
@@ -7,16 +7,17 @@ LANG_MAP = {
     "csharp":            ("csharp", "cs"),
 }
 
-choice = "{{ cookiecutter.language }}"
+choice = "csharp"
 print("HELLO THERE!!!!")
 print(choice) # <- ask() or argparse, etc.
 slug, abbrev = LANG_MAP[choice]
 
-cookiecutter(
-    "gh:wjb-dev/cookiecutter-fin-api",
-    no_input=False,
-    extra_context={
-        "variant":    slug,
-        "language_abbrev":  abbrev,
-    },
-)
+print(get_user_config().values())
+# cookiecutter(
+#     "gh:wjb-dev/cookiecutter-fin-api",
+#     no_input=False,
+#     extra_context={
+#         "variant":    slug,
+#         "language_abbrev":  abbrev,
+#     },
+# )
